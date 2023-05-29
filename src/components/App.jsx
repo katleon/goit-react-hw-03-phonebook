1e5ad6b1413c49ac4d9765313e7c27a4eb4894ef
Katarzyna Leoniuk <kasia.leoniuk@gmail.com>
2023-05-29 18:32:51 +0200

    Added: localstorage for contacts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,12 +7,7 @@ import Filter from './Filter/Filter';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
@@ -58,6 +53,21 @@ class App extends Component {
       contacts: prevState.contacts.filter(contact => contact.id !== todoId),
     }));
   };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   render() {
     const { contacts, filter } = this.state;
